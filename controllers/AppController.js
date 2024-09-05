af6bd8f57764e0b1d62a873e61f78b2355189a1d
creeds-knight <apedoarthur21@gmail.com>
2024-09-05 16:31:22 +0300

    created express server, routes, controller

---
+++ controllers/AppController.js
@@ -0,0 +1,21 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([noOfUsers, noOFiles]) => {
+        res.status(200).json({
+          users: noOfUsers,
+          files: noOFiles,
+        });
+      });
+  }
+}
